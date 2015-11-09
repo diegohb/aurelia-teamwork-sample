5446dafb9a47bb881f8c34457bd2584e4afdfba2
bustamd1 <bustamd1@.(none)>
2015-11-09 18:33:05 -0500

    -add bower components -add aurelia config file

---
+++ src/MMG.Utils.TWPM.HelperSPA/aurelia-config.js
@@ -0,0 +1,21 @@
+﻿import {LogManager} from "aurelia-framework";
+import {ConsoleAppender} from "aurelia-logging-console";
+import {$} from "bower_components/jquery/dist/jquery.js";
+
+LogManager.addAppender(new ConsoleAppender());
+LogManager.setLevel(LogManager.logLevel.debug);
+
+export function configure(aurelia) {
+    $("#pleaseWaitDialog").modal();
+
+    aurelia.use
+        .defaultBindingLanguage()
+        .defaultResources()
+        .history()
+        .router()
+        .eventAggregator();
+      /*.plugin("custom-plugin");*/
+
+    aurelia.start().then(a => a.setRoot("app", document.body))
+        .then(p => { $("#pleaseWaitDialog").modal("hide"); });
+}
