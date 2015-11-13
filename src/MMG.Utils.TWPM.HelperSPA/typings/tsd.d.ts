53c887e5806ee71b38e6c8361ca6c29ad47fbacc
bustamd1 <bustamd1@.(none)>
2015-11-13 01:21:15 -0500

    tsd bootstrap

--- src/MMG.Utils.TWPM.HelperSPA/typings/tsd.d.ts
+++ src/MMG.Utils.TWPM.HelperSPA/typings/tsd.d.ts
@@ -2,3 +2,5 @@
 /// <reference path="gruntjs/gruntjs.d.ts" />
 /// <reference path="node/node.d.ts" />
 /// <reference path="core-js/core-js.d.ts" />
+/// <reference path="bootstrap/bootstrap.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
