74d5a68f527f2347e96501661fb821835e8bd09f
bustamd1 <bustamd1@.(none)>
2015-11-13 01:18:07 -0500

    tsd core-js

--- src/MMG.Utils.TWPM.HelperSPA/typings/tsd.d.ts
+++ src/MMG.Utils.TWPM.HelperSPA/typings/tsd.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="es6-promise/es6-promise.d.ts" />
 /// <reference path="gruntjs/gruntjs.d.ts" />
 /// <reference path="node/node.d.ts" />
+/// <reference path="core-js/core-js.d.ts" />
