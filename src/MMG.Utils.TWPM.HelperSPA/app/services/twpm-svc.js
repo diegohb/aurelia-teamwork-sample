56dd61593e3c0328fbadcbab9e1f89294972af94
bustamd1 <bustamd1@.(none)>
2015-11-16 03:37:17 -0500

    updated js files

--- src/MMG.Utils.TWPM.HelperSPA/app/services/twpm-svc.js
+++ src/MMG.Utils.TWPM.HelperSPA/app/services/twpm-svc.js
@@ -3,13 +3,21 @@ export class TWPMService {
     constructor() {
         this.BaseURL = "https://mmgct.teamwork.com";
         this.httpClient = new HttpClient();
-        var apiToken = "horse965dry:password";
-        var base64auth = btoa(apiToken);
         this.httpClient.configure(config => {
             config.withBaseUrl(this.BaseURL);
-            config.withHeader("Authorization", "BASIC " + base64auth)
-                .withHeader("Accept", "application/json");
+            config.withHeader("Accept", "application/json");
         });
+    }
+    setApiToken(pApiToken) {
+        var apiToken = `${pApiToken}:password`;
+        var base64Auth = btoa(apiToken);
+        this.httpClient.configure(config => {
+            config.withHeader("Authorization", "BASIC " + base64Auth);
+        });
+        this.PartyID = this.getSelfPartyID();
+    }
+    getSelfPartyID() {
+        return 22762;
     }
     fetchTasks() {
         return this.httpClient.get("tasks.json?responsible-party-ids=22762&filter=today&sort=duedate");
