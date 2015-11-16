35bc44a8a54ab87ad75e52b4a861685100396052
bustamd1 <bustamd1@.(none)>
2015-11-16 02:39:06 -0500

    -deleted corejs after retargeting build settings for typescript to ecmascript6 (no module) -deleted es6 -renamed main to ts -added typed defs for aurelia

--- src/MMG.Utils.TWPM.HelperSPA/typings/tsd.d.ts
+++ src/MMG.Utils.TWPM.HelperSPA/typings/tsd.d.ts
@@ -1,8 +1,7 @@
-/// <reference path="es6-promise/es6-promise.d.ts" />
 /// <reference path="node/node.d.ts" />
-/// <reference path="core-js/core-js.d.ts" />
 /// <reference path="bootstrap/bootstrap.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="gulp/gulp.d.ts" />
 /// <reference path="orchestrator/orchestrator.d.ts" />
 /// <reference path="q/Q.d.ts" />
+/// <reference path="aurelia/framework@0.17.0/aurelia-framework.d.ts"/>
