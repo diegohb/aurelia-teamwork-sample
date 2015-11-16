810bade554cd06fab07716535cb13f14a23c2f53
bustamd1 <bustamd1@.(none)>
2015-11-16 02:57:51 -0500

    added new ts files (forgot)

---
+++ src/MMG.Utils.TWPM.HelperSPA/app/app.ts
@@ -0,0 +1,22 @@
+﻿import {TWPMService} from "./twpm-svc";
+
+export class App {
+    twpmService: TWPMService;
+    myTasks: typeof undefined[];
+
+    constructor() {
+        this.twpmService = new TWPMService();
+        this.myTasks = [];
+    }
+
+    activate() {
+        return this.twpmService.fetchTasks().then(response => {
+            if (!response.isSuccess)
+                throw new Error("Bad request from TeamworkPM.");
+
+            this.myTasks = response.content["todo-items"];
+        });
+    }
+
+    
+}
---
+++ src/MMG.Utils.TWPM.HelperSPA/app/twpm-svc.ts
@@ -0,0 +1,28 @@
+﻿import {HttpClient} from "aurelia-http-client";
+
+export class TWPMService {
+
+    BaseURL: string;
+    httpClient: HttpClient;
+
+    constructor () {
+        this.BaseURL = "https://mmgct.teamwork.com";
+        this.httpClient = new HttpClient();
+        
+        var apiToken = "horse965dry:password";
+        var base64auth = btoa(apiToken);
+
+        this.httpClient.configure(config => {
+            config.withBaseUrl(this.BaseURL);
+            config.withHeader("Authorization", "BASIC " + base64auth)
+                .withHeader("Accept", "application/json");
+
+        });
+    }
+    
+    fetchTasks() {
+        return this.httpClient.get("tasks.json?responsible-party-ids=22762&filter=today&sort=duedate");
+    }
+
+    
+}
