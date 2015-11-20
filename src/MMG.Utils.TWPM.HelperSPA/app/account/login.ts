733869ce22a15854d1a958324a41f882e9075ee0
bustamd1 <bustamd1@.(none)>
2015-11-19 21:56:36 -0500

    -installed aurelia-router -removed generated js files -created account view and vm -rebased spa to app dir and fixed referenced relative dirs -fixed css for top padding

--- src/MMG.Utils.TWPM.HelperSPA/app/account/login.ts
+++ src/MMG.Utils.TWPM.HelperSPA/app/account/login.ts
@@ -1,15 +1,12 @@
 ﻿import {TWPMService} from "../services/twpm-svc";
 
-export class MyListVM {
+export class LoginVM {
     apiToken: string;
     twpmService: TWPMService;
-    myTasks: typeof undefined[];
-
     isAuthenticated: boolean;
 
     constructor () {
         this.twpmService = new TWPMService();
-        this.myTasks = [];
     }
 
     activate () {
@@ -20,14 +17,4 @@ export class MyListVM {
         this.twpmService.setApiToken(this.apiToken);
         this.isAuthenticated = true;
     }
-
-    loadTasks () {
-        return this.twpmService.fetchTasks().then(response => {
-            if (!response.isSuccess)
-                throw new Error("Bad request from TeamworkPM.");
-
-            this.myTasks = response.content["todo-items"];
-        });
-    }
-
 }
--- src/MMG.Utils.TWPM.HelperSPA/app/app.ts
+++ src/MMG.Utils.TWPM.HelperSPA/app/app.ts
@@ -1,3 +1,15 @@
-﻿export class App {
+﻿import {RouterConfiguration, Router} from "aurelia-router";
 
+export class App {
+    router: Router;
+
+    configureRouter (config: RouterConfiguration, router: Router) {
+        config.title = "Wish Teamwork Did That...";
+        config.map([
+            { route: ["", "account/login"], name: "account-login", moduleId: "account/login", nav: true, title: "Login" },
+            { route: "tasks/mine", name: "tasks-my", moduleId: "tasks/mylist", nav: true, title: "My Tasks" }
+        ]);
+
+        this.router = router;
+    }
 }
