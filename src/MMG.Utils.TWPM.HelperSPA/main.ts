c7a0d0d00db71845044d98e0994f3c3c035368d8
bustamd1 <bustamd1@.(none)>
2015-11-24 21:04:30 -0500

    removed double-logging

--- src/MMG.Utils.TWPM.HelperSPA/main.ts
+++ src/MMG.Utils.TWPM.HelperSPA/main.ts
@@ -11,7 +11,7 @@ export function configure (aurelia) {
 
     aurelia.use
         .defaultBindingLanguage()
-        .developmentLogging()
+        /*.developmentLogging()*/
         .defaultResources()
         .history()
         .router()
