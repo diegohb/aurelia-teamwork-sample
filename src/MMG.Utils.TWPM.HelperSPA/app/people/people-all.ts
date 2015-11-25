ed4c75b379ce518ebef2d6871ff36f062a340641
bustamd1 <bustamd1@.(none)>
2015-11-24 22:25:05 -0500

    -added hasTitle and title property to Person. -hide title since it messes up layout

--- src/MMG.Utils.TWPM.HelperSPA/app/people/people-all.ts
+++ src/MMG.Utils.TWPM.HelperSPA/app/people/people-all.ts
@@ -30,6 +30,8 @@ export class PersonVM {
     lastName: string;
     avatarUrl: string;
     profileWebURL: string;
+    title: string;
+    hasTitle: boolean;
 
     constructor (pData: Person, pProfileWebURLBase: string) {
         this.personID = pData.personID;
@@ -37,7 +39,8 @@ export class PersonVM {
         this.lastName = pData.lastName;
         this.avatarUrl = pData.avatarUrl;
         this.profileWebURL = `${pProfileWebURLBase}/${this.personID}`;
+        this.hasTitle = pData.title && pData.title.length > 0;
+        if (this.hasTitle)
+            this.title = pData.title;
     }
-
-
 }
