1e97b694882d64bb78591249762d715fce18df10
bustamd1 <bustamd1@.(none)>
2015-12-12 22:30:05 -0500

    -fixed router paths to absolute "app" -added app dir mapping to absolute

--- src/MMG.Utils.TWPM.HelperSPA/app/app.ts
+++ src/MMG.Utils.TWPM.HelperSPA/app/app.ts
@@ -6,9 +6,9 @@ export class App {
     configureRouter (config: RouterConfiguration, router: Router) {
         config.title = "Wish Teamwork Did That...";
         config.map([
-            { route: ["", "account/login"], name: "account-authenticate", moduleId: "./account/login", nav: true, title: "Login" },
-            { route: "tasks/mine", name: "tasks-my", moduleId: "./tasks/mylist", nav: true, title: "My Tasks" },
-            { route: "people/all", name: "people-all", moduleId: "./people/people-all", nav: true, title: "Peepz" }
+            { route: ["", "account/login"], name: "account-authenticate", moduleId: "app/account/login", nav: true, title: "Login" },
+            { route: "tasks/mine", name: "tasks-my", moduleId: "app/tasks/mylist", nav: true, title: "My Tasks" },
+            { route: "people/all", name: "people-all", moduleId: "app/people/people-all", nav: true, title: "Peepz" }
         ]);
 
         this.router = router;
