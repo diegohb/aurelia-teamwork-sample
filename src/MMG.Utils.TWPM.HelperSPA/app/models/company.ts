7f855791244b2b3ab92dfd73263f23a4ab835bc9
bustamd1 <bustamd1@.(none)>
2015-12-14 16:19:25 -0500

    -added project model -added company model

---
+++ src/MMG.Utils.TWPM.HelperSPA/app/models/company.ts
@@ -0,0 +1,9 @@
+﻿export class Company {
+    ID: number;
+    name: string;
+
+    constructor (pData: any) {
+        this.ID = pData.id;
+        this.name = pData.name;
+    }
+}
---
+++ src/MMG.Utils.TWPM.HelperSPA/app/models/project.ts
@@ -0,0 +1,21 @@
+﻿import {Company} from "company";
+
+export class Project {
+    ID: number;
+    name: string;
+    lastModified: any;
+    dateCreated: any;
+    endDate: any;
+    company: Company;
+
+    constructor (pData: any) {
+        this.ID = pData.ID;
+        this.name = name;
+        this.lastModified = pData["last-changed-on"];
+        this.dateCreated = pData["created-on"];
+        this.endDate = pData.endDate;
+        if (pData.company) {
+            this.company = new Company(pData.company);
+        }
+    }
+}
