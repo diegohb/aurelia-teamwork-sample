2045d0260796a2b415b6e830406d5af10bf7a113
bustamd1 <bustamd1@.(none)>
2015-12-14 16:05:59 -0500

    -add view, vm -added to router config

--- src/MMG.Utils.TWPM.HelperSPA/app/app.ts
+++ src/MMG.Utils.TWPM.HelperSPA/app/app.ts
@@ -7,6 +7,7 @@ export class App {
         config.title = "Wish Teamwork Did That...";
         config.map([
             { route: ["", "account/login"], name: "account-authenticate", moduleId: "app/account/login", nav: true, title: "Login" },
+            { route: "projects/all", name: "project-all", moduleId: "app/projects/project-list", nav: true, title: "All Projects" },
             { route: "tasks/mine", name: "tasks-my", moduleId: "app/tasks/mylist", nav: true, title: "My Tasks" },
             { route: "people/all", name: "people-all", moduleId: "app/people/people-all", nav: true, title: "Peepz" }
         ]);
---
+++ src/MMG.Utils.TWPM.HelperSPA/app/projects/project-list.ts
@@ -0,0 +1,15 @@
+﻿import {TWPMService} from "app/services/twpm-svc";
+
+export class ProjectListVM {
+    private _twpmService: TWPMService;
+
+    /*private _projects: Array<Project>;*/
+
+    constructor () {
+        this._twpmService = new TWPMService();
+    }
+
+    activate (): Promise<any> {
+        return Promise.resolve();
+    }
+}
