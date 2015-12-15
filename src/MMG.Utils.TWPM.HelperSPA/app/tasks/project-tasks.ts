bec0521db3b13d40856673d6d0f93b963b954e97
bustamd1 <bustamd1@.(none)>
2015-12-15 14:45:04 -0500

    -added view for tasks by project -added route -added link to route by name

---
+++ src/MMG.Utils.TWPM.HelperSPA/app/tasks/project-tasks.ts
@@ -0,0 +1,19 @@
+﻿import {Project} from "app/models/project";
+
+export class TasksByProjectVM {
+    private _projectID: number;
+    private _project: Project;
+
+    constructor () {
+
+    }
+
+    activate (pActivationData: any) {
+        var projectID = parseInt(pActivationData.ProjectID);
+        this._projectID = projectID;
+    }
+
+    get ProjectName (): number { return this._projectID; }
+
+
+}
