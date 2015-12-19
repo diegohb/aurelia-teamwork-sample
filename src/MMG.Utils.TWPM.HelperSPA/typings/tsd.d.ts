4321c04df5d30f867b4ef7a739f806a12d9c6951
bustamd1 <bustamd1@.(none)>
2015-12-18 21:34:01 -0500

    -fixed typings for router def file -added pipeline step for auth check. -renamed typing directory. -removed tsd file.

--- src/MMG.Utils.TWPM.HelperSPA/typings/tsd.d.ts
+++ src/MMG.Utils.TWPM.HelperSPA/typings/tsd.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="bootstrap/bootstrap.d.ts" />
 /// <reference path="aurelia/framework@0.17.0/aurelia-framework.d.ts"/>
 /// <reference path="aurelia/fetch-client@1.0.0-beta.1.0.1/aurelia-fetch-client.d.ts" />
+/// <reference path="aurelia/aurelia-router@1.0.0-beta.1/aurelia-router.d.ts" />
