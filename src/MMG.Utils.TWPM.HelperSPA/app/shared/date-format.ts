db0f569325632e3c813fccd53bf6a0f7fdf1d8b1
bustamd1 <bustamd1@.(none)>
2016-02-17 23:26:05 -0500

    add value converter for moment date

---
+++ src/MMG.Utils.TWPM.HelperSPA/app/shared/date-format.ts
@@ -0,0 +1,8 @@
+﻿import * as moment from "moment";
+
+export class DateFormatValueConverter {
+    toView (value, format = "YYYY-MM-DD") {
+        var momentDate = value instanceof moment.constructor ? value : moment.constructor(value);
+        return momentDate.format(format);
+    }
+}
