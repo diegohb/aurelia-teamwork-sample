d03f88a127f781b48edac34b5b749e3f55818994
bustamd1 <bustamd1@.(none)>
2016-02-18 23:03:17 -0500

    added DateAge value converter and implemented.

--- src/MMG.Utils.TWPM.HelperSPA/app/shared/date-format.js
+++ src/MMG.Utils.TWPM.HelperSPA/app/shared/date-format.js
@@ -14,3 +14,27 @@ export class DateFormatValueConverter {
         return momentDate.format(format);
     }
 }
+
+export class DateAgeValueConverter {
+
+    toView (value, secondDateValue = new Date(), type = "past") {
+        var date1 = new Moment(value);
+        var date2 = new Moment(secondDateValue);
+
+        if (!date1.isValid())
+            return "";
+
+        if (!date2.isValid())
+            throw new Error("Invalid second date provided with '" + secondDateValue + "'.");
+
+        /*if (type !== "past" || type !== "future")
+            throw new Error("Invalid value for type parameter. Must be 'past' or 'future'.");*/
+
+        if (type === "past")
+            return date2.to(date1);
+        else if (type === "future")
+            return date1.to(date2);
+        else
+            return "unknown";
+    }
+}
