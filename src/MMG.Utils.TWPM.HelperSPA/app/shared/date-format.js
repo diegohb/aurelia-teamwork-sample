56cd7e6b7e62f175e62b71c025ceb41eb7a96b26
bustamd1 <bustamd1@.(none)>
2016-02-18 22:02:55 -0500

    chnaged value converter to JS because i couldn't figure out the typescript syntax to get momentjs to work.

---
+++ src/MMG.Utils.TWPM.HelperSPA/app/shared/date-format.js
@@ -0,0 +1,16 @@
+﻿//NOTE: the reason this is not a typescript file is because I didn't spend enought time 
+//trying to figure out how to get MomenJS to work correctly with TS compiler.
+//could be a problem with TS compiler i'm using right now or it could be resharper.
+
+import Moment from "moment";
+
+export class DateFormatValueConverter {
+
+    toView (value, format = "YYYY-MM-DD", defaultValue = "") {
+        if (!value)
+            return defaultValue;
+
+        var momentDate = value instanceof Moment ? value : new Moment(value);
+        return momentDate.format(format);
+    }
+}
--- src/MMG.Utils.TWPM.HelperSPA/app/shared/date-format.ts
+++
@@ -1,8 +0,0 @@
-﻿import * as moment from "moment";
-
-export class DateFormatValueConverter {
-    toView (value, format = "YYYY-MM-DD") {
-        var momentDate = value instanceof moment.constructor ? value : moment.constructor(value);
-        return momentDate.format(format);
-    }
-}
