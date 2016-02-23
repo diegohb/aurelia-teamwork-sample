4cfd0f7c3f819e5319c5a4f78e33920345e92c8b
bustamd1 <bustamd1@.(none)>
2016-02-22 20:01:13 -0500

    -added route for training tasks view -added training view and vm -implemented get tasks by tag

---
+++ src/MMG.Utils.TWPM.HelperSPA/app/tasks/training.ts
@@ -0,0 +1,29 @@
+﻿import {autoinject} from "aurelia-framework";
+import {TWPMService} from "app/services/twpm-svc";
+import {AuthState} from "app/services/auth-state";
+import {ListTaskItemVM as TaskVM} from "./viewmodels/list-task-vm";
+
+@autoinject()
+export class MyListVM {
+    twpmService: TWPMService;
+    myTasks: Array<TaskVM>;
+
+    isAuthenticated: boolean;
+
+    constructor (pTWPMService: TWPMService, private authState: AuthState) {
+        this.twpmService = pTWPMService;
+        this.myTasks = [];
+    }
+
+    activate () {
+        return this.loadTasks();
+    }
+
+    loadTasks () {
+        this.authState.ensureAuthenticated();
+        return this.twpmService.fetchTasksByTag("hr-training").then(tasks => {
+            this.myTasks = tasks.map(pTask => new TaskVM(pTask, this.authState.getInstallUrl()));
+        });
+    }
+
+}
