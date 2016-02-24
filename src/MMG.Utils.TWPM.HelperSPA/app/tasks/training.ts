aa382cb5d07ee79e44f9bdf9a18d164113596787
bustamd1 <>
2016-02-24 13:20:29 -0500

    -implemented k-tree-list component to view.. -added kendoTaskSource init to vm -not returning any data!

--- src/MMG.Utils.TWPM.HelperSPA/app/tasks/training.ts
+++ src/MMG.Utils.TWPM.HelperSPA/app/tasks/training.ts
@@ -1,33 +1,50 @@
-﻿import {autoinject} from "aurelia-framework";
+﻿///<reference path="../../typings/kendo-ui/kendo-ui.d.ts"/>
+
+import {autoinject} from "aurelia-framework";
 import {TWPMService} from "app/services/twpm-svc";
 import {AuthState} from "app/services/auth-state";
 import {ListTaskItemVM as TaskVM} from "./viewmodels/list-task-vm";
+
 
 @autoinject()
 export class MyListVM {
     twpmService: TWPMService;
     myTasks: Array<TaskVM>;
+    kendoTaskSource: any;
 
     isAuthenticated: boolean;
 
     constructor (pTWPMService: TWPMService, private authState: AuthState) {
         this.twpmService = pTWPMService;
         this.myTasks = [];
+        this.kendoTaskSource = null;
     }
 
     activate () {
+        this.kendoTaskSource = {
+            data: this.myTasks,
+            schema: {
+                model: {
+                    id: "TaskID"
+                }
+            }
+        };
+
         return this.loadTasks();
     }
 
     loadTasks () {
-        this.authState.ensureAuthenticated();
+        var self = this;
+        self.authState.ensureAuthenticated();
         //tag 'hr-training' id = 800
-        return this.twpmService.fetchTasksByTag([800]).then(tasks => {
-            this.myTasks = tasks.map(pTask => new TaskVM(pTask, this.authState.getInstallUrl()))
+        return self.twpmService.fetchTasksByTag([800]).then(tasks => {
+            self.myTasks = tasks.map(pTask => new TaskVM(pTask, self.authState.getInstallUrl()))
                 .sort((pItem1, pItem2) => {
                     return pItem1.AssigneeDisplay.localeCompare(pItem2.AssigneeDisplay); //sort by assignee, respect non-ASCII chars
                     //ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
                 });
+
+            self.kendoTaskSource.read();
         });
     }
 
