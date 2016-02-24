7f61cfd88149bf5f8d14bf719a4e944aa8964d78
bustamd1 <bustamd1@.(none)>
2016-02-23 22:38:44 -0500

    -add new typing references -deleted old typed definitions

--- src/MMG.Utils.TWPM.HelperSPA/typings/tsd.d.ts
+++ src/MMG.Utils.TWPM.HelperSPA/typings/tsd.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="node/node.d.ts" />
 /// <reference path="bootstrap/bootstrap.d.ts" />
-/// <reference path="aurelia/framework@0.17.0/aurelia-framework.d.ts"/>
-/// <reference path="aurelia/fetch-client@1.0.0-beta.1.0.1/aurelia-fetch-client.d.ts" />
-/// <reference path="aurelia/aurelia-router@1.0.0-beta.1/aurelia-router.d.ts" />
+/// <reference path="aurelia/aurelia-framework@1.0.0-beta.1.0.8/aurelia-framework.d.ts"/>
+/// <reference path="aurelia/aurelia-fetch-client@1.0.0-beta.1.0.2/aurelia-fetch-client.d.ts" />
+/// <reference path="aurelia/aurelia-router@1.0.0-beta.1.0.1/aurelia-router.d.ts" />
+/// <reference path="aurelia/aurelia-templating@1.0.0-beta.1.0.3/aurelia-templating.d.ts" />
