246e4cd5c13247ff32fa79c50826ae3f639368ef
diegohb <>
2017-02-09 17:42:57 -0500

    bundle output

--- src/MMG.Utils.TWPM.HelperSPA_v3/scripts/app-bundle.js
+++ src/MMG.Utils.TWPM.HelperSPA_v3/scripts/app-bundle.js
@@ -426,5 +426,269 @@ define('twpm/auth-state',["require", "exports", "aurelia-dependency-injection", 
     exports.AuthState = AuthState;
 });
 
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+define('twpm/twpm-auth',["require", "exports", "aurelia-framework", "./twpm-client-factory", "./auth-state", "../models/auth-info"], function (require, exports, aurelia_framework_1, twpm_client_factory_1, auth_state_1, auth_info_1) {
+    "use strict";
+    var TWPMAuthService = (function () {
+        function TWPMAuthService(pClientFactory, authState) {
+            this.authState = authState;
+            this.clientFactory = pClientFactory;
+        }
+        TWPMAuthService.prototype.authenticate = function (pApiToken) {
+            var _this = this;
+            this.httpClient = this.clientFactory.createApiClient(pApiToken);
+            return this.getAuthUserInfo()
+                .then(function (pAuthInfo) {
+                _this.authState.validateApiToken(pApiToken, pAuthInfo);
+                return {
+                    Success: true,
+                    Account: pAuthInfo
+                };
+            }).catch(function (err) {
+                return { Success: false, ErrorMessage: err.message, Error: err };
+            });
+        };
+        TWPMAuthService.prototype.getAuthUserInfo = function () {
+            return this.httpClient.fetch("https://authenticate.teamwork.com/authenticate.json")
+                .then(function (pResponse) {
+                if (!pResponse.ok)
+                    throw new Error("Bad request to TeamworkPM.");
+                var promiseData = pResponse.json();
+                return promiseData.then(function (pData) { return auth_info_1.AuthUserInfo.parse(pData.account); });
+            }).catch(function (err) {
+                var translatedError;
+                switch (err.statusCode) {
+                    case 401:
+                        translatedError = new Error("Invalid API Token!");
+                        break;
+                    default:
+                        translatedError = new Error("Unspecified error - " + err.statusText);
+                        break;
+                }
+                return Promise.reject(translatedError);
+            });
+        };
+        TWPMAuthService.prototype.endAuthSession = function () {
+            this.authState.reset();
+            console.log("Logged out and reset AuthState!");
+        };
+        return TWPMAuthService;
+    }());
+    TWPMAuthService = __decorate([
+        aurelia_framework_1.autoinject(),
+        __metadata("design:paramtypes", [twpm_client_factory_1.TWPMClientFactory, auth_state_1.AuthState])
+    ], TWPMAuthService);
+    exports.TWPMAuthService = TWPMAuthService;
+});
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
+    return { next: verb(0), "throw": verb(1), "return": verb(2) };
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+define('twpm/twpm-svc',["require", "exports", "aurelia-framework", "./twpm-client-factory", "./auth-state", "../models/project", "../models/task", "../models/person", "../models/twfile"], function (require, exports, aurelia_framework_1, twpm_client_factory_1, auth_state_1, project_1, task_1, person_1, twfile_1) {
+    "use strict";
+    var TWPMService = (function () {
+        function TWPMService(pClientFactory, authState) {
+            this.authState = authState;
+            this.apiClient = pClientFactory.createApiClient(this.authState.apiToken);
+        }
+        TWPMService.prototype.fetchPerson = function (pPersonID) {
+            return __awaiter(this, void 0, void 0, function () {
+                var personID;
+                return __generator(this, function (_a) {
+                    personID = pPersonID || this.authState.userInfo.userID;
+                    return [2 /*return*/, this.apiClient.fetch("people/" + personID + ".json")
+                            .then(this.getJson).then(function (pData) {
+                            return person_1.Person.parse(pData);
+                        })];
+                });
+            });
+        };
+        TWPMService.prototype.fetchPeople = function () {
+            return __awaiter(this, void 0, void 0, function () {
+                return __generator(this, function (_a) {
+                    return [2 /*return*/, this.apiClient.fetch("people.json")
+                            .then(this.getJson).then(function (pData) {
+                            var materializedPeople = pData.people.map(function (pPersonRaw) { return person_1.Person.parse(pPersonRaw); });
+                            return materializedPeople;
+                        })];
+                });
+            });
+        };
+        TWPMService.prototype.fetchAllProjects = function () {
+            return __awaiter(this, void 0, void 0, function () {
+                var _this = this;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, this.apiClient.fetch("projects.json").then(function (pResponse) {
+                                if (pResponse.ok)
+                                    return _this.getJson(pResponse);
+                                return Promise.reject(pResponse.error());
+                            })
+                                .then(function (pData) {
+                                return pData.projects.map(function (pItem) { return project_1.Project.parse(pItem); });
+                            })];
+                        case 1: return [2 /*return*/, _a.sent()];
+                    }
+                });
+            });
+        };
+        TWPMService.prototype.fetchProjectByID = function (pProjectID) {
+            return __awaiter(this, void 0, void 0, function () {
+                var requestURL;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            requestURL = "projects/" + pProjectID + ".json";
+                            return [4 /*yield*/, this.apiClient.fetch(requestURL).then(this.getJson)
+                                    .then(function (pData) {
+                                    return project_1.Project.parse(pData.project);
+                                })];
+                        case 1: return [2 /*return*/, _a.sent()];
+                    }
+                });
+            });
+        };
+        TWPMService.prototype.fetchTasks = function (pPartyID) {
+            return __awaiter(this, void 0, void 0, function () {
+                var _this = this;
+                var partyID, requestURL;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            partyID = pPartyID || this.authState.userInfo.userID;
+                            requestURL = "tasks.json?responsible-party-ids=" + partyID + "&filter=today&sort=duedate";
+                            return [4 /*yield*/, this.apiClient.fetch(requestURL)
+                                    .then(function (response) {
+                                    if (!response.ok)
+                                        throw new Error("Bad request from TeamworkPM.");
+                                    return _this.getJson(response).then(function (pData) {
+                                        var items = pData["todo-items"];
+                                        return items.map(function (pItem) { return new task_1.Task(pItem); });
+                                    });
+                                })];
+                        case 1: return [2 /*return*/, _a.sent()];
+                    }
+                });
+            });
+        };
+        TWPMService.prototype.fetchTasksByTag = function (pTags) {
+            return __awaiter(this, void 0, void 0, function () {
+                var _this = this;
+                var requestURL;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            requestURL = "tasks.json?tag-ids=" + pTags.join(",") + "&sort=duedate&includeCompletedTasks=true";
+                            return [4 /*yield*/, this.apiClient.fetch(requestURL)
+                                    .then(function (response) {
+                                    if (!response.ok)
+                                        throw new Error("Bad request from TeamworkPM.");
+                                    return _this.getJson(response).then(function (pData) {
+                                        var items = pData["todo-items"];
+                                        return items.map(function (pItem) { return new task_1.Task(pItem); });
+                                    });
+                                })];
+                        case 1: return [2 /*return*/, _a.sent()];
+                    }
+                });
+            });
+        };
+        TWPMService.prototype.fetchTasksByProject = function (pProjectID) {
+            return __awaiter(this, void 0, void 0, function () {
+                var requestURL;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            requestURL = "projects/" + pProjectID + "/tasks.json?includeCompletedTasks=true";
+                            return [4 /*yield*/, this.apiClient.fetch(requestURL).then(this.getJson)
+                                    .then(function (pData) {
+                                    var rawTasks = pData["todo-items"];
+                                    return rawTasks.map(function (pItem) { return new task_1.Task(pItem); });
+                                })];
+                        case 1: return [2 /*return*/, _a.sent()];
+                    }
+                });
+            });
+        };
+        TWPMService.prototype.fetchFilesByProject = function (pProjectID) {
+            return __awaiter(this, void 0, void 0, function () {
+                var requestURL, installURL;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            requestURL = "projects/" + pProjectID + "/files.json";
+                            installURL = this.authState.getInstallUrl();
+                            return [4 /*yield*/, this.apiClient.fetch(requestURL).then(this.getJson)
+                                    .then(function (pData) {
+                                    var rawFiles = pData.project.files;
+                                    return rawFiles.map(function (pItem) { return twfile_1.TWFile.parse(pItem, installURL); });
+                                })];
+                        case 1: return [2 /*return*/, _a.sent()];
+                    }
+                });
+            });
+        };
+        TWPMService.prototype.getJson = function (pResponse) {
+            return pResponse.json();
+        };
+        return TWPMService;
+    }());
+    TWPMService = __decorate([
+        aurelia_framework_1.transient(),
+        aurelia_framework_1.autoinject(),
+        __metadata("design:paramtypes", [twpm_client_factory_1.TWPMClientFactory, auth_state_1.AuthState])
+    ], TWPMService);
+    exports.TWPMService = TWPMService;
+});
+
 define('text!app.html', ['module'], function(module) { module.exports = "<template><h1>${message}</h1></template>"; });
 //# sourceMappingURL=app-bundle.js.map
