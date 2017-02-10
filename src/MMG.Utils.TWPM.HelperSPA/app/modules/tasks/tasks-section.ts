bf72211d189656aacebd762485ad000bfdbd5267
diegohb <>
2017-02-09 23:31:17 -0500

    add tasks section

--- src/MMG.Utils.TWPM.HelperSPA/app/modules/tasks/tasks-section.ts
+++ src/MMG.Utils.TWPM.HelperSPA/app/modules/tasks/tasks-section.ts
@@ -1,11 +1,10 @@
-﻿/**
-* The shell for the projects  section of the application.  Will contain either
-* the project-list or project page.
-*/
-export class TaskSection {
+﻿export class TaskSection {
     configureRouter(config, router) {
         config.map([
             { route: "", moduleId: "./task-list", nav: false, title: "" },
+            { route: "tasks/mine", moduleId: "./mylist", nav: true, title: "My Tasks" },
+            { route: "tasks/training", moduleId: "./training", nav: true, title: "Training Tasks" },
+            { route: "tasks/by-project", moduleId: "./project-tasks", nav: true, title: "Project Tasks" },
             { route: ":id", moduleId: "./task", nav: false, title: "" }
         ]);
     }
